e410880fc32297720ca286ac8bb27296e4ab6ac9
Victus <abdulbositkhalikov96@gmail.com>
2024-03-18 23:27:38 +0500

    UI improvements

--- app/src/main/java/uz/doston/e_learn/ui/theme/Color.kt
+++ app/src/main/java/uz/doston/e_learn/ui/theme/Color.kt
@@ -4,8 +4,8 @@ import androidx.compose.ui.graphics.Color
 
 val Primary = Color(0xFF3AB766)
 val Secondary = Color(0xFF66D3E3)
-val Background = Color(0xFFB4E5F9)
+val Background = Color(0xFFEEE4B1)
 val TextColor = Color(0xFF0E4A5E)
-val Blue = Color(0xFF0579CE)
-val Green = Color(0xFF35853A)
-val Red = Color(0xFFAD231C)
+val Blue = Color(0xFF387ADF)
+val Green = Color(0xFF9BCF53)
+val Red = Color(0xFFFF204E)
